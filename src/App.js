d7b731b425b2b681e8b21222da4b0f65bf7ecc14
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-04-04 19:33:33 -0500

    App switches pages now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import Employee from './Employee';
+
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const navigateToEmployee = () => {
+    // 👇️ navigate to /contacts
+    navigate('/Employee');
+  };
+
   return (
     <>
 
       <div>
         <button>Manager</button>
-        <button>Employee</button>
+        <button onClick={navigateToEmployee}>Employee</button>
+
+        <Routes>
+          <Route path='/Employee' element={<Employee/>} />
+        </Routes>
+
       </div>
     
     </>
