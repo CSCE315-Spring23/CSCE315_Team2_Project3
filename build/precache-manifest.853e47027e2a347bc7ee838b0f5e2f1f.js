38202f432133e1f8a7208008857f1e18e51d3c33
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-05-02 20:37:32 -0500

    Required commit

---
+++ build/precache-manifest.853e47027e2a347bc7ee838b0f5e2f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6cc18bdc5a636f54451",
+    "url": "/static/css/main.c190f155.chunk.css"
+  },
+  {
+    "revision": "b6cc18bdc5a636f54451",
+    "url": "/static/js/main.145248c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "522b0420bd4158be7e80",
+    "url": "/static/js/2.c5242134.chunk.js"
+  },
+  {
+    "revision": "1f81b5014f6ef486b1835a7d0ad82d71",
+    "url": "/static/media/GothamRounded-BoldItalic.1f81b501.otf"
+  },
+  {
+    "revision": "3d9ee5a5bb7325c50e0c490c3c6924fa",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36b48c8757d8ba3c22289f85319b753f.js"
+  "/precache-manifest.853e47027e2a347bc7ee838b0f5e2f1f.js"
 );
 
 workbox.clientsClaim();
