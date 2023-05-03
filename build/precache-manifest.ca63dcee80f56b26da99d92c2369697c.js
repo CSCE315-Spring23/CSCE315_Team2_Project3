48fab0718d3a3bb40ab66cb8959c355e09e6f346
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-05-02 21:25:07 -0500

    Required commit

---
+++ build/precache-manifest.ca63dcee80f56b26da99d92c2369697c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "612a098b7240b2509b9b",
+    "url": "/static/css/main.b31c60fd.chunk.css"
+  },
+  {
+    "revision": "612a098b7240b2509b9b",
+    "url": "/static/js/main.4bf71501.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49f53632becaa3e4198e",
+    "url": "/static/js/2.82becead.chunk.js"
+  },
+  {
+    "revision": "e8b68bc6238af2051b673c6a1557276b",
+    "url": "/index.html"
+  }
+];
